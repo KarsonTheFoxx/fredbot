ebc024188926075839ae68e99c2cfb3b4036a6fe
KarsonTheFoxx <95767655+KarsonTheFoxx@users.noreply.github.com>
2024-09-08 22:08:37 -0600

    Version 1.1.1 removed debugging print statements

--- plugins/automatic_tasks.py
+++ plugins/automatic_tasks.py
@@ -7,47 +7,47 @@ ORIGINS_MESSAGE = None
 COUNCIL_MESSAGE = None
 plugin = plugins.Plugin()
 
-# @plugin.register_loop()
-# @tasks.loop(seconds=30)
-# async def check_status():
-#     await plugin.bot.wait_until_ready()
-#     global ORIGINS_MESSAGE, COUNCIL_MESSAGE
-#     embed = Embed()
-#     timestamp = f":t:{int(datetime.now().timestamp())}:r:"
-#     try:
-#         server = BedrockServer("OriginsBE.duckdns.org", 19225)
-#         status = server.status()
-#         embed.title = ":green_circle: Server is online"
-#         embed.description = f"Version: `{status.version}`\nPlayers: `{status.players_online}/{status.players_max}`\nlatency (from west Canada)): `{status.latency}`\n-# Last updated: {timestamp}"
-#         embed.color = Color.green()
-#     except TimeoutError:
-#         try:
-#             server = BedrockServer("54.38.29.227", 19225)
-#             status = server.status()
-#             embed.title = ":yellow_circle: Server is online"
-#             embed.description = f" DuckDNS unreachable, use IP: `54.38.29.227` instead.\nVersion: `{status.version}`\nPlayers: `{status.players_online}/{status.players_max}`\nlatency (from west Canada)): `{status.latency}`\n-# Last updated: {timestamp}"
-#             embed.color = Color.yellow()
-#         except TimeoutError:
-#             embed.title = ":red_circle: Server unreachable"
-#             embed.description = f"Server is offline, or incorrect address has been passed\n-# Last updated: {timestamp}"
-#             embed.color = Color.red()
-#             embed.add_field(name="Adresses tried", value="`OriginsBE.duckdns.org:19255`, `54.38.29.227:19255`")
+@plugin.register_loop()
+@tasks.loop(seconds=30)
+async def check_status():
+    await plugin.bot.wait_until_ready()
+    global ORIGINS_MESSAGE, COUNCIL_MESSAGE
+    embed = Embed()
+    timestamp = f":t:{int(datetime.now().timestamp())}:r:"
+    try:
+        server = BedrockServer("OriginsBE.duckdns.org", 19225)
+        status = server.status()
+        embed.title = ":green_circle: Server is online"
+        embed.description = f"Version: `{status.version}`\nPlayers: `{status.players_online}/{status.players_max}`\nlatency (from west Canada)): `{status.latency}`\n-# Last updated: {timestamp}"
+        embed.color = Color.green()
+    except TimeoutError:
+        try:
+            server = BedrockServer("54.38.29.227", 19225)
+            status = server.status()
+            embed.title = ":yellow_circle: Server is online"
+            embed.description = f" DuckDNS unreachable, use IP: `54.38.29.227` instead.\nVersion: `{status.version}`\nPlayers: `{status.players_online}/{status.players_max}`\nlatency (from west Canada)): `{status.latency}`\n-# Last updated: {timestamp}"
+            embed.color = Color.yellow()
+        except TimeoutError:
+            embed.title = ":red_circle: Server unreachable"
+            embed.description = f"Server is offline, or incorrect address has been passed\n-# Last updated: {timestamp}"
+            embed.color = Color.red()
+            embed.add_field(name="Adresses tried", value="`OriginsBE.duckdns.org:19255`, `54.38.29.227:19255`")
     
-#     try:
-#         if ORIGINS_MESSAGE == None:
-#             ORIGINS_MESSAGE = await plugin.bot.get_guild(826685546772168734).get_channel(1161528232965836931).send(embed=embed)
-#         else:
-#             await ORIGINS_MESSAGE.edit(embed=embed)
-#     except Exception:
-#         pass
-#     try:
-#         print(COUNCIL_MESSAGE)
-#         if COUNCIL_MESSAGE == None:
-#             COUNCIL_MESSAGE = await plugin.bot.get_guild(1166799644362276967).get_channel(1282534413430689963).send(embed=embed)
-#         else:
-#             await COUNCIL_MESSAGE.edit(embed=embed)
-#     except Exception:
-#         pass
+    try:
+        if ORIGINS_MESSAGE == None:
+            ORIGINS_MESSAGE = await plugin.bot.get_guild(826685546772168734).get_channel(1161528232965836931).send(embed=embed)
+        else:
+            await ORIGINS_MESSAGE.edit(embed=embed)
+    except Exception:
+        pass
+    try:
+        print(COUNCIL_MESSAGE)
+        if COUNCIL_MESSAGE == None:
+            COUNCIL_MESSAGE = await plugin.bot.get_guild(1166799644362276967).get_channel(1282534413430689963).send(embed=embed)
+        else:
+            await COUNCIL_MESSAGE.edit(embed=embed)
+    except Exception:
+        pass
 
             
 setup, teardown = plugin.create_extension_handlers()
